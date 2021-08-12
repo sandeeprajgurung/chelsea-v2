db90b1f90a7b9ce8af5e426065658f9a857d5c87
Bibash-24 <bbus24.a@gmail.com>
2021-08-12 15:21:50 +0545

    navbar changes

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -113,6 +113,7 @@
                                   <li><a href="blog.html">Blog</a></li>
                                   <li><a href="shop.html">Shop</a></li>
                                   <li><a href="contact.html">Contact</a></li>
+                                  <li><a href="prediction.html">Predctions</a></li>
                                </ul>
                             </div>
                             <!-- /.nav-collapse -->
