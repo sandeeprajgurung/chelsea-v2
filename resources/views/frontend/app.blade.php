8b23267e14129aac1cc503a1681771482d5a39cb
sandeeprajgurung <smile2srg@gmail.com>
2021-08-12 09:56:58 +0545

    frontend changes

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -11,6 +11,7 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/default.css') }}" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
