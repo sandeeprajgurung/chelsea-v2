5ca871d84eb6de0539038b4480c2f80866f28112
Bibash-24 <bbus24.a@gmail.com>
2021-08-12 15:28:55 +0545

    added favicon

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chelsea Nepal</title>
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
